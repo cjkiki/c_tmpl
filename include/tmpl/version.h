a8db9e7d95ed7781d00f584d0a9885bd648e4b22
kiki <christianjuric187@gmail.com>
2026-04-07 10:02:37 +0200

    Implemented entry point

---
+++ include/tmpl/version.h
@@ -0,0 +1,9 @@
+#ifndef TMPL_VERSION_H
+#define TMPL_VERSION_H
+
+#define TMPL_VERSION_MAJOR 0
+#define TMPL_VERSION_MINOR 1
+#define TMPL_VERSION_PATCH 0
+#define TMPL_VERSION "0.1.0"
+
+#endif // TMPL_VERSION_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "tmpl/version.h"
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("Version: %s\n", TMPL_VERSION);
+    return 0;
+}
